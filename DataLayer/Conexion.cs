9873f4920d26b8243895928160c513d4260036e5
Tender Ticante <tenderticante@gmail.com>
2018-02-05 16:10:21 -0800

    Creacion de la capa de datos para los proveedores

---
+++ DataLayer/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Conexion
+    {
+        public static string CadenaConexion = "Data Source=DESKTOP-8L7GQC9; Initial Catalog= dbToolCrib; Integrated Security=true";
+    }
+}

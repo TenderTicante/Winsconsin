06bd451729227e8e7a8bb9b1a4e28dde1384c6ab
Tender Ticante <tenderticante@gmail.com>
2018-02-11 14:21:14 -0800

    Creacion de la clase de estructura para la tabla Centro de Costos

--- DataLayer/CentroCostosData.cs
+++ DataLayer/CentroCostosData.cs
@@ -10,7 +10,7 @@ using System.Data.SqlClient;
 
 namespace DataLayer
 {
-    class CentroCostosData
+    public class CentroCostosData
     {
         //Variables para hacer referencia a los de la tabla
         private int _ClaveCC;
---
+++ StructLayer/CentroCostoStruct.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Importacion de los recursos de la capa de Datos
+using DataLayer;
+using System.Data;
+
+namespace StructLayer
+{
+    public class CentroCostoStruct
+    {
+        //Metodo para llamar a la funcion Insertar que esta en la capa de datos
+        public static string Insertar(int clavecc, string nombrecc, string mse)
+        {
+            CentroCostosData CCD = new CentroCostosData();
+
+            CCD.ClaveCC = clavecc;
+            CCD.Nombre = nombrecc;
+            CCD.MSE = mse;
+         
+            return CCD.Insertar(CCD);
+        }
+
+        //Metodo para llamar a la funcion Editar que esta en la capa de datos
+
+        public static string Editar(int clavecc, string nombrecc, string mse)
+        {
+            CentroCostosData CCD = new CentroCostosData();
+
+            CCD.ClaveCC = clavecc;
+            CCD.Nombre = nombrecc;
+            CCD.MSE = mse;
+
+            return CCD.Insertar(CCD);
+        }
+
+        //Metodo para llamar a la funcion Eliminar que esta en la capa de datos
+
+        public static string Eliminar(int clavecc)
+        {
+            CentroCostosData CCD = new CentroCostosData();
+
+            CCD.ClaveCC = clavecc;
+
+            return CCD.Insertar(CCD);
+        }
+
+        //Metodo para llamar a la funcion Mostrar que esta en la capa de datos
+
+        public static DataTable Mostrar()
+        {
+
+            return new CentroCostosData().Mostrar();
+        }
+
+        //Metodo de Busqueda de Clave en la capa de datos
+
+        public static DataTable BuscarClaveCC(string var)
+        {
+            CentroCostosData CCD = new CentroCostosData();
+            CCD.AuxTxt = var;
+            return CCD.Busqueda(CCD);
+        }
+    }
+}
